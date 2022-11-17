f4364882b2fd313a34bf0e2f5d9d00c02524901d
Juansitoh <juanfergamer77@gmail.com>
2022-11-17 09:10:43 -0500

    17 de noviembre de 2022 - Finalización de diseño del carrito de compras.

--- AMJOR-Node.js-main/app.js
+++ AMJOR-Node.js-main/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config
 const express = require ('express')
 const hbs = require('hbs')
 const app = express()
@@ -6,81 +7,76 @@ const port = 4000
 app.use(express.static('public'))
 
 app.get('/',(req, res) => {
-    res.render('home',{
+    res.render('home/home',{
         nombre: 'Home'
     })
 });
 
-hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+hbs.registerPartials(__dirname + '/views/home/partials', function (err) {});
 hbs.registerPartials(__dirname + '/views/dashboard/partialsDash', function (err) {});
 app.set('view engine', 'hbs');
 app.set('views',__dirname + '/views');
 
 app.get('/contacto', (req,res) =>{
     
-    res.render('contact' ,{
+    res.render('home/contact' ,{
         nombre:'Contacto'
     })
 })
 
 app.get('/login', (req,res) =>{
     
-    res.render('login' ,{
+    res.render('home/login' ,{
         nombre:'Login'
     })
 })
 app.get('/registro', (req,res) =>{
     
-    res.render('registro' ,{
+    res.render('home/registro' ,{
         nombre:'Registrarse'
     })
 })
 
 app.get('/about', (req,res) =>{
     
-    res.render('about' ,{
+    res.render('home/about' ,{
         nombre:'About'
     })
 })
-app.get('/head', (req,res) =>{
-    
-    res.render('head' ,{
-        nombre:'Head'
-    })
-})
+
 app.get('/cart', (req,res) =>{
     
-    res.render('cart' ,{
+    res.render('home/cart' ,{
         nombre:'Cart'
     })
 })
 app.get('/checkout', (req,res) =>{
     
-    res.render('checkout' ,{
+    res.render('home/checkout' ,{
         nombre:'Checkout'
     })
 })
 app.get('/shop', (req,res) =>{
     
-    res.render('shop' ,{
+    res.render('home/shop' ,{
         nombre:'Shop'
     })
 })
 app.get('/shop-single', (req,res) =>{
     
-    res.render('shop-single' ,{
+    res.render('home/shop-single' ,{
         nombre:'detalleProducto'
     })
 })
 app.get('/contact', (req,res) =>{
     
-    res.render('contact' ,{
+    res.render('home/contact' ,{
         nombre:'Contacto'
     })
 })
 app.get('/thankyou', (req,res) =>{
     
-    res.render('thankyou' ,{
+    res.render('home/thankyou' ,{
         nombre:'Chek'
     })
 })
@@ -97,9 +93,21 @@ app.get('/pedidos',(req,res) =>{
     })
 })
 
-app.get('/productos',(req,res) =>{
-    res.render('dashboard/productos' ,{
-        nombre:'Pedidos'
+app.get('/anchetas',(req,res) =>{
+    res.render('dashboard/anchetas' ,{
+        nombre:'Anchetas'
+    })
+})
+
+app.get('/insumos',(req,res) =>{
+    res.render('dashboard/insumos' ,{
+        nombre:'Insumos'
+    })
+})
+
+app.get('/configuracion',(req,res) =>{
+    res.render('dashboard/configuracion' ,{
+        nombre:'configuracion'
     })
 })
 
@@ -111,12 +119,10 @@ app.get('/usuarios',(req,res) =>{
 
 app.get('*',(req,res) =>{
 
-    res.render('404' ,{
+    res.render('home/404' ,{
         nombre:'pagina no encontrada'
     })
 })
-
-
 
 app.listen(port,() =>{
     console.log(`escuchando por el puerto http://localhost:${port}`)
